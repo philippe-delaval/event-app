60ee46fd36bb4bfc95a3d9f0753fdafcff733975
David Moreau <moi@david-moreau.fr>
2023-08-30 15:08:59 +0200

    feat: create events migration

---
+++ knexfile.ts
@@ -0,0 +1,39 @@
+import type { Knex } from "knex";
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      host: "localhost",
+      database: "postgres",
+      user: "postgres",
+      password: "example",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  // TODO add production config for first deployment
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+module.exports = config;
