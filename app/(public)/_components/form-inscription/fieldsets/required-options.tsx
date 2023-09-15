68b5ef9c8f20dde079aa77b1905c7337b7b805e5
DAVID R <103181452+drondiodev@users.noreply.github.com>
2023-09-15 12:06:51 +0200

    add mld file

---
+++ app/(public)/_components/form-inscription/fieldsets/required-options.tsx
@@ -0,0 +1,11 @@
+import Approuval from "../fields/approuval";
+import MaterialRules from "../fields/material-rules";
+
+export default function RequiredOptions() {
+  return (
+    <>
+      <MaterialRules />
+      <Approuval />
+    </>
+  );
+}
--- app/(public)/_components/form-inscription/form-inscription.tsx
+++ app/(public)/_components/form-inscription/form-inscription.tsx
@@ -1,16 +1,9 @@
-'use client'
+"use client";
 
-import FirstName from './fields/firstname';
-import LastName from './fields/lastname';
-import Email from './fields/email';
-import MaterialRules from './fields/material-rules';
-import Approuval from './fields/approuval';
-import Categories from './fields/categories';
-import Languages from './fields/languages';
-import Experiences from './fields/experiences';
+import FirstName from "./fields/firstname";
+import LastName from "./fields/lastname";
 
 export default function FormInscription() {
-
   return (
     <div className="bg-background-grey pb-10">
       <div className="mx-auto max-w-7xl p-4 sm:p-6 lg:px-8">
@@ -26,51 +19,34 @@ export default function FormInscription() {
           </div>
         </div>
         <div className="mx-auto max-w-3xl">
-            <form>
-              <div className="space-y-12 sm:space-y-16">
-                <div>
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">
-                    Informations personnelles
-                  </h2>
-                  <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
-                    Assurez-vous de fournir des informations précises et
-                    complètes dans le formulaire afin de recevoir un billet
-                    valide pour l&#39;événement.
-                  </p>
-                  <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
-                    <FirstName />
-                    <LastName />
-                    <Email />
-                  </div>
-                </div>
-                <div>
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">
-                    Informations professionnelles
-                  </h2>
-                  <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
-                    Veuillez fournir des informations professionnelles et
-                    techniques exactes et complètes.
-                  </p>
-                  <div className="mt-10 space-y-10 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
-                    <Categories />
-                    <Languages />
-                    <Experiences />
-                  </div>
+          <form>
+            <div className="space-y-12 sm:space-y-16">
+              <div>
+                <h2 className="text-base font-semibold leading-7 text-gray-900">
+                  Informations personnelles
+                </h2>
+                <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-600">
+                  Assurez-vous de fournir des informations précises et complètes
+                  dans le formulaire afin de recevoir un billet valide pour
+                  l&#39;événement.
+                </p>
+                <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+                  <FirstName />
+                  <LastName />
                 </div>
               </div>
-              <div className="mt-6">
-                <MaterialRules />
-                <Approuval />
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                  <button
-                    type="submit"
-                    className="rounded-md bg-primary-orange px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary-orange focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-                  >
-                    Valider mon inscription et générer mon e-billet
-                  </button>
-                </div>
+            </div>
+            <div className="mt-6">
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <button
+                  type="submit"
+                  className="rounded-md bg-primary-orange px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-secondary-orange focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                >
+                  Valider mon inscription et générer mon e-billet
+                </button>
               </div>
-            </form>
+            </div>
+          </form>
         </div>
       </div>
     </div>
