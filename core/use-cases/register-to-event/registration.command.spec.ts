ac9be37033f34aea6de0200be760a5d0037176d6
David Moreau <moi@david-moreau.fr>
2023-09-22 11:21:16 +0200

    refactor: registration command tests concision

--- core/use-cases/register-to-event/registration.command.spec.ts
+++ core/use-cases/register-to-event/registration.command.spec.ts
@@ -1,150 +1,74 @@
 import { ZodError } from "zod";
 import { RegistrationCommand } from "./registration.command";
 
-describe("First name field validation", () => {
-  it("rejects a first name that is too short", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "F",
-          lastName: "Doe",
-        })
-    ).toThrow(ZodError);
+describe("RegistrationCommand validation", () => {
+  const validFirstNames = ["John", "Anne-Marie"];
+
+  const invalidFirstNames = [
+    { name: "F", reason: "too short" },
+    { name: "A".repeat(251), reason: "too long" },
+    { name: "Bob@", reason: "invalid characters" },
+    { name: "", reason: "empty" },
+    { name: "Foo1", reason: "contains numbers" },
+  ];
+
+  const validLastNames = ["Doe", "De-la-Villardière"];
+
+  const invalidLastNames = [
+    { name: "D", reason: "too short" },
+    { name: "A".repeat(251), reason: "too long" },
+    { name: "Doe@", reason: "invalid characters" },
+    { name: "", reason: "empty" },
+    { name: "Bar1", reason: "contains numbers" },
+  ];
+
+  describe("First name validations", () => {
+    test.each(validFirstNames)(
+      "accepts a valid first name like %p",
+      (firstName) => {
+        expect(
+          () =>
+            new RegistrationCommand({
+              firstName,
+              lastName: "Doe",
+            })
+        ).not.toThrow();
+      }
+    );
+
+    test.each(invalidFirstNames)("rejects a first name %p", ({ name }) => {
+      expect(
+        () =>
+          new RegistrationCommand({
+            firstName: name,
+            lastName: "Doe",
+          })
+      ).toThrow(ZodError);
+    });
   });
 
-  it("rejects a first name that is too long", () => {
-    const longName = "A".repeat(251);
+  describe("Last name validations", () => {
+    test.each(validLastNames)(
+      "accepts a valid last name like %p",
+      (lastName) => {
+        expect(
+          () =>
+            new RegistrationCommand({
+              firstName: "John",
+              lastName,
+            })
+        ).not.toThrow();
+      }
+    );
 
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: longName,
-          lastName: "Doe",
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("accepts a first name with valid special characters (hyphens and apostrophes...)", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Anne-Marie",
-          lastName: "Doe",
-        })
-    ).not.toThrow();
-  });
-
-  it("rejects a first name containing special characters", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Bob@",
-          lastName: "Doe",
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("rejects when the first name is missing", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "",
-          lastName: "Bar",
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("rejects when the first name contains numbers", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Foo1",
-          lastName: "Bar",
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("validates when the first name contains spaces around", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: " Foo ",
-          lastName: "Bar",
-        })
-    ).not.toThrow();
-  });
-});
-
-describe("Last name field validation", () => {
-  it("rejects a last name that is too short", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Foo",
-          lastName: "D",
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("rejects a last name that is too long", () => {
-    const longName = "A".repeat(251);
-
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Foo",
-          lastName: longName,
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("accepts a last name with valid special characters (hyphens and apostrophes...)", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Anne-Marie",
-          lastName: "De-la-Villardière",
-        })
-    ).not.toThrow();
-  });
-
-  it("rejects a last name containing special characters", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Bob",
-          lastName: "Doe@",
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("rejects when the last name is missing", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Foo",
-          lastName: "",
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("rejects when the last name contains numbers", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Foo",
-          lastName: "Bar1",
-        })
-    ).toThrow(ZodError);
-  });
-
-  it("validates when the last name contains spaces around", () => {
-    expect(
-      () =>
-        new RegistrationCommand({
-          firstName: "Foo",
-          lastName: " Bar ",
-        })
-    ).not.toThrow();
+    test.each(invalidLastNames)("rejects a last name %p", ({ name }) => {
+      expect(
+        () =>
+          new RegistrationCommand({
+            firstName: "John",
+            lastName: name,
+          })
+      ).toThrow(ZodError);
+    });
   });
 });
