7d2b550b9353e296751c19fdc1fb9aa24711cd0e
klareg <klareg@gmail.com>
2023-09-25 16:36:51 +0200

    feat: add test check email already registered

---
+++ migrations/20230925140919_add_email_unicity_constraint.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("attendees", (table) => {
+    table.string("email").unique().alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return;
+};
