531656a88932c6a65fa704c7cfa4df99d9cf1dd3
David Moreau <moi@david-moreau.fr>
2023-09-28 18:28:30 +0200

    refactor: send registration confirmation email

--- src/core/use-cases/register-to-event/register-to-event.use-case.ts
+++ src/core/use-cases/register-to-event/register-to-event.use-case.ts
@@ -21,7 +21,7 @@ export async function registerToEventUseCase(
   const { attendeesRepository, registrationsRepository, emailSender } =
     dependencies;
 
-  await checkEmailUnicity(attendeesRepository, command);
+  await checkEmailUnicity(attendeesRepository, command.email);
 
   const registrationConfirmationHash = new RegistrationConfirmationHash(
     command.email,
@@ -38,25 +38,19 @@ export async function registerToEventUseCase(
     },
   );
 
-  await emailSender.send({
-    to: command.email,
-    subject: "Veuillez confirmer votre inscription à l'événement Test Event",
-    html: `
-          <p>
-            Pour la confirmer, veuillez cliquer sur le lien suivant : 
-            <a href="http://localhost:3000/confirmer-inscription/${registrationConfirmationHash.toString()}">Confirmer l'inscription</a>
-          </p>
-        `,
-  });
+  await sendRegistrationConfirmationEmail(
+    emailSender,
+    command.email,
+    registrationConfirmationHash,
+  );
 }
 
 async function checkEmailUnicity(
   attendeesRepository: AttendeesRepository,
-  command: RegistrationCommand,
+  attendeeEmail: string,
 ) {
-  const attendeeWithEmail = await attendeesRepository.findByEmail(
-    command.email,
-  );
+  const attendeeWithEmail =
+    await attendeesRepository.findByEmail(attendeeEmail);
 
   if (attendeeWithEmail !== null) {
     throw new AttendeeEmailAlreadyRegisteredError();
@@ -85,5 +79,22 @@ async function saveAttendeeAndRegistration(
     attendee_id: attendeeId,
     event_id: 1,
     confirmation_hash: registrationConfirmationHash.toString(),
+  });
+}
+
+async function sendRegistrationConfirmationEmail(
+  emailSender: EmailSender,
+  attendeeEmail: string,
+  registrationConfirmationHash: RegistrationConfirmationHash,
+) {
+  await emailSender.send({
+    to: attendeeEmail,
+    subject: "Veuillez confirmer votre inscription à l'événement Test Event",
+    html: `
+          <p>
+            Pour la confirmer, veuillez cliquer sur le lien suivant : 
+            <a href="http://localhost:3000/confirmer-inscription/${registrationConfirmationHash.toString()}">Confirmer l'inscription</a>
+          </p>
+        `,
   });
 }
